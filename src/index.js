42a9d0a2935dc00c796d7578dc38cbdd408fce4e
David A Pears <davidapears@gmail.com>
2019-10-15 09:59:29 +0100

    Refactors to add simple MUTATION

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 let links = [
   {
     id: "link-0",
@@ -22,7 +8,6 @@ let links = [
   }
 ];
 
-// 2
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
@@ -34,9 +19,9 @@ const resolvers = {
     url: parent => parent.url
   }
 };
-// 3
+
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
